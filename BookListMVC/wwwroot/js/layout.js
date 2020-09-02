277523ce7d8dd66c9ee0771dd7bea59ca20f3c36
Marco Sowa <sowa@plt-software.de>
2020-09-02 10:21:23 +0200

    minor refactoring

--- BookListMVC/wwwroot/js/layout.js
+++ BookListMVC/wwwroot/js/layout.js
@@ -3,21 +3,13 @@ const themeCheckboxValue = JSON.parse(localStorage.getItem('themeCheckboxValue')
 const isChecked = JSON.parse(localStorage.getItem("themeCheckboxValue"));
 
 document.getElementById("toggle-theme-checkbox").checked = isChecked;
-if (themeCheckBox.checked) {
-  document.documentElement.setAttribute('data-theme', 'dark')
-} else {
-  document.documentElement.setAttribute('data-theme', 'light')
-}
+toggleTheme();
 
 $(document).ready(function () {
   themeCheckBox.addEventListener('change', function () {
     trans()
     localStorage.setItem("themeCheckboxValue", JSON.stringify(this.checked));
-    if (this.checked) {
-      document.documentElement.setAttribute('data-theme', 'dark')
-    } else {
-      document.documentElement.setAttribute('data-theme', 'light')
-    }
+    toggleTheme();
   })
 });
 
@@ -26,4 +18,12 @@ function trans() {
   window.setTimeout(() => {
     document.documentElement.classList.remove('transition')
   }, 300)
+}
+
+function toggleTheme() {
+  if (themeCheckBox.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark')
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light')
+  }
 }
