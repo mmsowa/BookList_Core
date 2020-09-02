784c889b8c278e0abaa8f497d821cb26fa6e32aa
Marco Sowa <sowa@plt-software.de>
2020-09-02 09:57:46 +0200

    Save theme in localStorage

--- BookListMVC/wwwroot/js/layout.js
+++ BookListMVC/wwwroot/js/layout.js
@@ -1,5 +1,13 @@
 ﻿const themeCheckBox = document.querySelector('input[name=theme]');
-let themeCheckboxValue = JSON.parse(localStorage.getItem('themeCheckboxValue')) || {};
+const themeCheckboxValue = JSON.parse(localStorage.getItem('themeCheckboxValue')) || {};
+const isChecked = JSON.parse(localStorage.getItem("themeCheckboxValue"));
+
+document.getElementById("toggle-theme-checkbox").checked = isChecked;
+if (themeCheckBox.checked) {
+  document.documentElement.setAttribute('data-theme', 'dark')
+} else {
+  document.documentElement.setAttribute('data-theme', 'light')
+}
 
 $(document).ready(function () {
   themeCheckBox.addEventListener('change', function () {
